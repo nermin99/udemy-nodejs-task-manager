9febe922f4d6d09e1244b53f822cd559f32166b8
Nermin Skenderovic <nesk@netcompany.com>
2023-10-03 15:51:53 +0200

    Add a '/' endpoint listing all endpoints

--- src/app.js
+++ src/app.js
@@ -1,13 +1,19 @@
 const express = require('express')
 require('./db/mongoose')
+const log = require('./middleware/log')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
-const log = require('./middleware/log')
 
 const app = express()
 
+const routes = [...userRouter.stack, ...taskRouter.stack].map((layer) => {
+  const method = Object.keys(layer.route.methods)[0]
+  return `${method.toUpperCase()} ${layer.route.path}`
+})
+
 app.use(express.json())
 app.use(log)
+app.get('/', (req, res) => res.send(routes))
 app.use(userRouter)
 app.use(taskRouter)
 
